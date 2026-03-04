620181f6c6e85acae9aed1eb6d8a0a274ea913f2
halogen <charlieleavitt23@gmail.com>
2026-03-04 11:44:05 -0500

    fix merge conflicts, build issues

--- cmake/LinuxCrosscompile.cmake
+++ cmake/LinuxCrosscompile.cmake
@@ -10,6 +10,8 @@ set(CMAKE_CXX_COMPILER_FRONTEND_VARIANT MSVC)
 add_compile_options(/winsysroot /opt/msvc --target=x86_64-pc-windows-msvc -fms-compatibility -fms-extensions -fdelayed-template-parsing)
 add_link_options(/winsysroot:/opt/msvc)
 
+#set(CMAKE_RC_COMPILE_OBJECT "<CMAKE_RC_COMPILER> /I /opt/msvc/kits/10/include/10.0.26100.0/shared <DEFINES> /fo<OBJECT> <SOURCE>")
+
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
